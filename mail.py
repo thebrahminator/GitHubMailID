3196bdad013a28847cc4be872f671e79754174cb
thebrahminator <vishwa.nath@outlook.com>
2017-03-27 01:08:50 +0530

    Organisation's structure taken into account

--- mail.py
+++ mail.py
@@ -15,9 +15,18 @@ for data in raw_data:
     if data['actor']['login'] != name:
         print("It's an organisation's name")
         exit(0)
-    if data['type'] == 'PushEvent':
-        email = data['payload']['commits'][0]['author']['email']
-        flag = 1
+
+    if flag != 1:
+        if data['type'] == 'PushEvent':
+            email = data['payload']['commits'][0]['author']['email']
+            flag = 1
+
+    if "org" in data.keys():
+        if data['type'] == 'PushEvent':
+            #print(data['payload']['commits'])
+            for commit in data['payload']['commits']:
+               email = commit['author']['email']
+               flag = 1
 
 if flag == 0:
     print("Unable to retrieve Mail ID")
