4367a08fdac8b5d9e3db34e07fb08a313063f874
thebrahminator <vishwa.nath@outlook.com>
2017-03-27 05:35:47 +0530

    Refined organisation mail ID search

--- mail.py
+++ mail.py
@@ -12,28 +12,29 @@ if not raw_data:
     exit(0)
 flag = 0
 email1 = []
+email = "filler"
 for data in raw_data:
     if data['actor']['login'] != name:
         print("It's an organisation's name")
         exit(0)
 
-    if flag != 1:
-        if data['type'] == 'PushEvent':
-            email = data['payload']['commits'][0]['author']['email']
-            flag = 1
-
     if "org" in data.keys():
         if data['type'] == 'PushEvent':
-            #print(data['payload']['commits'])
             for commit in data['payload']['commits']:
                email1.append(commit['author']['email'])
                flag = 1
 
+    if "org" not in data.keys():
+        if data['type'] == 'PushEvent':
+            email = data['payload']['commits'][0]['author']['email']
+            flag = 1
+
 if flag == 0:
     print("Unable to retrieve Mail ID")
 else:
-    if not email1:
+    if email != "filler":
         print("The email ID is : " + email)
+
     else:
         print("Conflicting mail IDs, the list of mail IDs are\n")
         for mail in email1:
